38e06498b801deaa013bb9e6f6efd03d9a505ebf
trandanggiatoai-2000 <trandanggiatoai@gmail.com>
2026-05-02 15:49:17 +0700

    Cap nhat bao mat

--- Areas/Admin/Controllers/AdminAuthController.cs
+++ Areas/Admin/Controllers/AdminAuthController.cs
@@ -41,7 +41,7 @@ public class AdminAuthController : Controller
             return RedirectToLocal(returnUrl);
         }
 
-        model.ErrorMessage = "Sai tài khoản hoặc mật khẩu. Tài khoản mặc định: admin / 123456";
+        model.ErrorMessage = "Sai tài khoản hoặc mật khẩu.";
         return View(model);
     }
 
